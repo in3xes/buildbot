1bff2208d4e67bcd7455fd69bd725cc472157cc1
Pradeepkumar Gayam <in3xes@gmail.com>
2011-08-15 12:58:27 +0530

    Started with tests

---
+++ master/buildbot/test/unit/test_steps_source_bzr.py
@@ -0,0 +1,55 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+from twisted.trial import unittest
+from buildbot.steps.source import bzr
+from buildbot.status.results import SUCCESS
+from buildbot.test.util import sourcesteps
+from buildbot.test.fake.remotecommand import ExpectShell, ExpectLogged
+
+class TestBzr(sourcesteps.SourceStepMixin, unittest.TestCase):
+
+    def setUp(self):
+        return self.setUpSourceStep()
+
+    def tearDown(self):
+        return self.tearDownSourceStep()
+
+    def test_mode_full_clean(self):
+        self.setupStep(
+            bzr.Bzr(repourl='http://bzr.squid-cache.org/bzr/squid3/trunk',
+                    mode='full', method='clean'))
+        self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['bzr', '--version'])
+            + 0,
+            ExpectLogged('stat', dict(file='wkdir/.bzr',
+                                      logEnviron=True))
+            + 0,
+            ExpectShell(workdir='wkdir',
+                        command=['bzr', 'clean-tree', '--ignored', '--force'])
+            + 0,
+            ExpectShell(workdir='wkdir',
+                        command=['bzr', 'update'])
+            + 0,
+            ExpectShell(workdir='wkdir',
+                        command=['bzr', 'version-info', '--custom', "--template='{revno}"])
+            + ExpectShell.log('stdio',
+                stdout='100')
+            + 0,
+                    )
+        self.expectOutcome(result=SUCCESS, status_text=["update"])
+        return self.runStep()
+
